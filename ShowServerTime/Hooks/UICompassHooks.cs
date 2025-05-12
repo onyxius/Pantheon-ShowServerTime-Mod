13487fc4fa40a5fd5485a3ddc9923af404eec88a
ModsOfPantheon <194365524+ModsOfPantheon@users.noreply.github.com>
2025-05-12 01:00:56 +0100

    Replace UIPanelHook with UICompass hook

--- ShowServerTime/Hooks/UICompassHooks.cs
+++ ShowServerTime/Hooks/UICompassHooks.cs
@@ -7,19 +7,16 @@ namespace ShowServerTime.Hooks;
 /// <summary>
 /// Hooks for UI panel events, specifically for creating the server time display
 /// </summary>
-[HarmonyPatch(typeof(UIWindowPanel), nameof(UIWindowPanel.Start))]
-public class UIPanelHooks
+[HarmonyPatch(typeof(UICompass), nameof(UICompass.Start))]
+public class UICompassHooks
 {
     /// <summary>
     /// Called when a UI panel starts
     /// Creates the server time display when the compass panel is initialized
     /// </summary>
     /// <param name="__instance">The UI panel that started</param>
-    private static void Postfix(UIWindowPanel __instance)
+    private static void Postfix(UICompass __instance)
     {
-        if (__instance.name == "Panel_Compass")
-        {
-            ShowServerTime.ModMain.CreateTimeDisplay(__instance.transform);
-        }
+        ModMain.CreateTimeDisplay(__instance.transform);
     }
 } 
